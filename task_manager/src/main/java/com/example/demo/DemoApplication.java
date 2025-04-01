e92a4fff92222f528773db34eddcc3fab4792a87
sudharsan2027vec <sudharsanlabvec@gmail.com>
2025-04-01 17:50:34 +0530

    Add files via upload

---
+++ task_manager/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@SpringBootApplication
+@Controller
+public class DemoApplication {
+
+    @GetMapping("/")
+    public String home() {
+        return "index.html";
+    }
+
+    @GetMapping("/signInUp")
+    public String signInUp() {
+        return "signInUp.html";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute SigninPage signinPage) {
+        System.out.println(signinPage.toString());
+        return "signInUp.html";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute SignupPage signupPage) {
+        System.out.println(signupPage.toString());
+        return "signInUp.html";
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
